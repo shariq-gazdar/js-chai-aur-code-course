235dd5b53796211d4e8c5b01a7b684e9fbbc0fcd
= <gazdarshariq0@gmail.com>
2024-09-16 12:03:20 +0500

    ...

---
+++ recap/index.js
@@ -0,0 +1,13 @@
+console.log();
+
+// Array Methods
+// let someArr = ["S", "H", 2, 3, 4];
+// for (const element of someArr) {
+//   console.log(element);
+// }
+
+// Objects Methods
+
+// for (const key in someArr) {
+//   console.log(key);
+// }
