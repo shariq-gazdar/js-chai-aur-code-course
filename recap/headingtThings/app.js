04b03feb1caf86ae3802273cf657f70d583e97c1
= <gazdarshariq0@gmail.com>
2024-09-18 23:20:14 +0500

    .......

--- recap/headingtThings/app.js
+++ recap/headingtThings/app.js
@@ -1,14 +1,14 @@
 let heading = document.querySelector(".heading");
 let color = "rgb(157,54,255)";
 document.addEventListener("mousemove", handlemouse);
-var divtag = document.querySelectorAll("div");
+var div = document.querySelectorAll("div");
 function handlemouse(event) {
   // console.log(event);
-  divtag[1].innerHTML = `X: ${event.clientX}`;
-  divtag[2].innerHTML = `Y: ${event.clientY}`;
-  divtag[0].style.position = "absolute";
-  divtag[0].style.top = `${event.clientY - 50}px`;
-  divtag[0].style.left = `${event.clientX - 50}px`;
+  div[1].innerHTML = `X: ${event.clientX}`;
+  div[2].innerHTML = `Y: ${event.clientY}`;
+  div[0].style.position = "absolute";
+  div[0].style.top = `${event.clientY - 50}px`;
+  div[0].style.left = `${event.clientX - 50}px`;
   heading.style.fontSize = `${(event.clientX + event.clientY) / 5}px`;
 
   heading.style.color = `rgb(157,54,${event.clientX / 5})`;
